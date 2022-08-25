ffee2960d5c2d6c8c7d80bab9451a344e993f965
fsclaudio <fsclaudio@gmail.com>
2022-08-24 21:13:08 -0300

    first component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+function NotificationButton() {
+    return (
+        <div className="ds-meta-red-btn">
+             <img src={icon} alt="Notificar" /> 
+        </div>
+    )
+}
+
+export default NotificationButton
